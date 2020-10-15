019c66b602f53e69cabf3c82f0773031208ce666
Mara <majer.mara@gmail.com>
2020-10-15 07:45:44 +0200

    proto service added

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Hadusak/binary_data_storage_API
 go 1.15
 
 require (
+	github.com/golang/protobuf v1.4.2
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/gorm v1.9.16
 )
--- pkg/server/grpcServer.go
+++ pkg/server/grpcServer.go
@@ -1,2 +1,16 @@
 package server
 
+import (
+	"github.com/Hadusak/binary_data_storage_API/pkg/utils"
+	"log"
+	"net"
+)
+
+func NewGRPCServer() {
+	lis, err := net.Listen("tcp", utils.GetEnv("PROTO_PORT", ":31744"))
+	if err != nil{
+		log.Fatalf("failed to listen %v", err)
+	}
+
+	proto
+}
